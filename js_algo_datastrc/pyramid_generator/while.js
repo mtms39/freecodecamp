9e0af71a1d17941d4cede8c7af2bd908ecfb7bdd
focalfossa <you@example.com>
2025-06-30 19:42:32 -0300

    Adjusting operator of equality in `while.js`

--- js_algo_datastrc/pyramid_generator/while.js
+++ js_algo_datastrc/pyramid_generator/while.js
@@ -1,13 +1,16 @@
 /* equality operator "==" if two variables are equal => "0" == 0 => true
  * strict equality operator "===" if two are equal and has the same type => "0" === 0 => false*/
-
-let continueLoop = false;
+let count = 8;
+let continueLoop = true;
 let done = 0;
+
+console.log("done variable before while loop: " + done + "\n");
 
 while (continueLoop) {
   done++;
-  if (done == count) {
+  if (done === count) {
+    continueLoop = false;
   }
 }
 
-console.log(done);
+console.log("done varaible after while loop: " + done + "\n");
