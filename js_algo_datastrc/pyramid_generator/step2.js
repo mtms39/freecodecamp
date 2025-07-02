29de70cafcbb3f97bf5c2804c96fc490e2aba8da
focalfossa <you@example.com>
2025-07-01 21:01:31 -0300

    remove some irrelevant files

--- js_algo_datastrc/pyramid_generator/step2.js
+++
@@ -1,8 +0,0 @@
-/*
- * Variable naming follows specific rules: names can include letters, numbers, dollar signs, and underscores, but cannot contain spaces and must not begin with a number.
- * Mutation is characteristic of non primitives data types, such as an array. It means it is mutable, can be changed, when otherwise, primitive data types can only be reassigned
- * A method is a funciton that is associated with certian variables and values
- * array.push() => add value to the end of the array
- * array.pop() => removes the last element from the array
- * /
-
